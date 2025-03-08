c6fb045143cb0af9754763c74a7236954664e756
deepxnshiiii <kuchchhal.deepanshi@gmail.com>
2025-03-08 12:49:27 +0530

    updated

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,3 +1,20 @@
 export default function PrivacyPolicy() {
-    return <div></div>;
+    return (
+        <div className="relative">
+            <h1 className="before:content-[''] before:absolute before:top-0 before:left-24 before:h-12 before:w-12 before:bg-transparent before:rounded-tl-3xl before:shadow-[0_-22px_0_0_#fff]"></h1>
+            <div className="absolute py-12 px-12 bg-white rounded-br-3xl"></div>
+            <h1 className="before:content-[''] before:absolute before:top-24 before:left-0 before:h-12 before:w-12 before:bg-transparent before:rounded-tl-3xl before:shadow-[0_-22px_0_0_#fff]"></h1>
+
+            <div className="mt-20 py-24 flex flex-col items-center bg-[#E0F7FF] text-[#003588] rounded-3xl">
+                <h3 className="text-[2.5rem]  font-semibold">
+                    Privacy Policy
+                </h3>
+                <p className="font-semibold">Last Updated: 28th November, 2024</p>
+            </div>
+
+            <h1 className="before:content-[''] before:absolute before:bottom-0 before:right-24 before:h-12 before:w-12 before:bg-transparent before:rounded-br-3xl before:shadow-[0_22px_0_0_#FFF]"></h1>
+            <div className="absolute bottom-0 right-0 py-12 px-12 bg-white rounded-tl-2xl"></div>
+            <h1 className="before:content-[''] before:absolute before:bottom-24 before:right-0 before:h-12 before:w-12 before:bg-transparent before:rounded-br-3xl before:shadow-[0_22px_0_0_#FFF]"></h1>
+        </div>
+    );
 }
